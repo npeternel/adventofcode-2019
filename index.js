c6fb2323ae8dd2d58c210fcbb3a1100cbe4bae92
Nicki Peternel <nicki.peternel@mapbox.com>
2019-12-08 17:00:15 -0800

    automate new day creation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 const solutions = {
   day1: require('./day1/solution'),
@@ -9,8 +10,33 @@ const solutions = {
 }
 
 if (require.main === module) {
-  const day = process.argv[2];
+  const day = process.argv[3];
   if (!/day\d+/.test(day)) { throw Error(`invalid day '${day}'`); }
-  const input = fs.readFileSync(__dirname + '/' + day + '/input', 'utf8');
-  solutions[day].solution(input);
+  switch (process.argv[2]) {
+    case 'new':
+      exec(`git checkout -b ${day}`, function(err){
+        if (err) throw err;
+      });
+      fs.mkdirSync(__dirname + '/' + day, '0755');
+      fs.writeFileSync(__dirname + '/' + day + '/input', '');
+      fs.writeFileSync(__dirname + '/' + day + '/solution.js', `'use strict';
+
+function solution(input) {
+  console.log(\`Answer is \${answer}\`);
+}
+      
+module.exports.solution = solution;
+`);
+      fs.writeFileSync(__dirname + '/' + day + '/solution.test.js', `'use strict';
+
+const test = require('tape');
+const { solution } = require('./solution');
+`);
+      break;
+    case 'run':
+      const input = fs.readFileSync(__dirname + '/' + day + '/input', 'utf8');
+      solutions[day].solution(input);
+      break;
+    default:
+  }
 }
