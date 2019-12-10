0c84651de76948c71f4e36081bd469e21ccd50ba
Nicki Peternel <nickipeternel@gmail.com>
2019-12-09 19:07:48 -0500

    day1 part 2 (#7)

--- day1/solution.js
+++ day1/solution.js
@@ -1,13 +1,33 @@
 'use strict';
 
 function solution(data) {
+  const part1Answer = part1(data);
+  const part2Answer = part2(data);
+  console.log(`Answer is ${part1Answer} and ${part2Answer}`);
+  return [part1Answer, part2Answer];
+}
+
+function part1(data) {
   let sum = 0;
   data.split('\n').forEach((mass) => {
     if (!mass) return;
     const massInt = parseInt(mass, 10);
     sum += Math.floor(massInt / 3) - 2;
   });
-  console.log(`Answer is ${sum}`);
+  return sum;
+}
+
+function part2(data) {
+  let sum = 0;
+  data.split('\n').forEach((mass) => {
+    if (!mass) return;
+    const currMass = parseInt(mass, 10);
+    let result = Math.floor(currMass / 3) - 2;
+    while (result > 0) {
+      sum += result;
+      result = Math.floor(result / 3) - 2;
+    }
+  });
   return sum;
 }
 
--- day1/solution.test.js
+++ day1/solution.test.js
@@ -4,10 +4,19 @@ const test = require('tape');
 const fs = require('fs');
 const { solution } = require('./solution');
 
-test('day1', (assert) => {
+test('day1 part 1', (assert) => {
   const input = fs.readFileSync(__dirname + '/testinput', 'utf8');
   const sum = solution(input);
-  assert.equals(sum, 34241, 'correct total fuel');
+  assert.equals(sum[0], 34241, 'correct total fuel');
   assert.end();
 });
 
+test('day1 part 2', (assert) => {
+  let sum = solution('14');
+  assert.equals(sum[1], 2, 'correct total fuel');
+  sum = solution('1969');
+  assert.equals(sum[1], 966, 'correct total fuel');
+  sum = solution('100756');
+  assert.equals(sum[1], 50346, 'correct total fuel');
+  assert.end();
+});
